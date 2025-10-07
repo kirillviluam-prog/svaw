8a38c3c74a951c32e04250f09dfac4fa7ad33aa1
kirillviluam-prog <kirillviluam@gmail.com>
2025-10-07 19:40:44 +0400

    Delete store.js

--- store.js
+++
@@ -1,8 +0,0 @@
-const storeItems = [
-    {name: 'Молоко', price: 5, minLevel: 1},
-    {name: 'Рыбка', price: 7, minLevel: 2},
-    {name: 'Кошачий корм', price: 3, minLevel: 2},
-    {name: 'Вода', price: 2, minLevel: 2},
-    {name: 'Бантик', price: 2, minLevel: 2},
-    {name: 'Погремушка', price: 3, minLevel: 2},
-    {name: 'Заводная мышка
